68528d8aacaa74307832c3ef1b8052cd31818c13
nastya-atro <belle.nastja@gmail.com>
2022-10-01 13:07:40 +0300

    connect with mysql, redis, impl session saving

--- dist/common/exeptions/all-exception.filter.d.ts
+++
@@ -1,8 +0,0 @@
-import { ArgumentsHost } from '@nestjs/common';
-import { BaseExceptionFilter, HttpAdapterHost } from '@nestjs/core';
-import { LoggerService } from '../logger/logger.service';
-export declare class AllExceptionFilter extends BaseExceptionFilter {
-    private logger;
-    constructor(adapterHost: HttpAdapterHost, logger: LoggerService);
-    catch(exception: any, host: ArgumentsHost): Promise<void>;
-}
--- dist/common/middleware/logger.middleware.d.ts
+++
@@ -1,2 +0,0 @@
-import { Request, Response, NextFunction } from 'express';
-export declare function logger(request: Request, response: Response, next: NextFunction): void;
